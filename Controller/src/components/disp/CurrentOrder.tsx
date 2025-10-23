9810e5440aade6c9beaaf9083ffdfd75646c43b1
mochirow <souma0822u@gmail.com>
2025-10-23 20:08:17 +0900

    アップデート

--- Controller/src/components/disp/CurrentOrder.tsx
+++ Controller/src/components/disp/CurrentOrder.tsx
@@ -11,6 +11,9 @@ type OrderNowItem = {
 };
 
 const orderManager = new OrderManager();
+
+orderManager.ReadOrder();
+
 const findNum = 1;
 
 export default function CurrentOrder() {
@@ -33,79 +36,36 @@ export default function CurrentOrder() {
   const total = _serial ? calcTotalFromOrder(_serial.order) : 0;
 
   return (
-    <div className="flex flex-col gap-4 h-screen">
+    <div className="flex flex-col h-screen">
       <div className="bg-amber-500 p-3 flex justify-center font-semibold sticky top-0 z-10">
         現在の注文
       </div>
+      <div className="w-full flex-1 flex flex-col items-start overflow-y-auto pb-40">
+        <div className="w-full flex flex-col items-start pb-3" id="order-list">
+          {orderManager.orderTable.at(-1)?.order.map((flag, index) => {
+            return (
+              <div className="w-full flex flex-col justify-start border-t-2 pl-1 pb-2">
+                <div className="font-bold">{index}. 焼うどん</div>
+                {orderManager.menu
+                  .filter((element) => {
+                    //なんか実行できん
+                    //return flag.HasFlag(element.Flag);
+                    return element.Flag == ((flag.flag ?? 0) & element.Flag);
+                  })
+                  .map((element, index) => (
+                    <div className="w-full flex flex-row justify-between border-t-1 border-dashed px-2 font-regular">
+                      <div key={index} className="flex">
+                        {element.Item}
+                      </div>
 
-      <div className="flex-1 flex flex-col items-start overflow-y-auto pb-40">
-        <div className="w-full flex flex-col items-start pb-3">
-          <div className="w-full flex flex-col justify-start border-t-2 pl-1">
-            {orderManager.menu.map((m, idx) => (
-              <div
-                key={idx}
-                className="w-full flex justify-between border-t-1 border-dashed px-2 py-1"
-              >
-                <span>{m.Item}</span>
-                <span className="font-normal">¥{m.Price}</span>
+                      <div key={index} className="flex">
+                        ￥{element.Price}
+                      </div>
+                    </div>
+                  ))}
               </div>
-            ))}
-          </div>
-          <div className="w-full flex flex-col justify-start border-t-2 pl-1">
-            {orderManager.menu.map((m, idx) => (
-              <div
-                key={idx}
-                className="w-full flex justify-between border-t-1 border-dashed px-2 py-1"
-              >
-                <span>{m.Item}</span>
-                <span className="font-normal">¥{m.Price}</span>
-              </div>
-            ))}
-          </div>
-          <div className="w-full flex flex-col justify-start border-t-2 pl-1">
-            {orderManager.menu.map((m, idx) => (
-              <div
-                key={idx}
-                className="w-full flex justify-between border-t-1 border-dashed px-2 py-1"
-              >
-                <span>{m.Item}</span>
-                <span className="font-normal">¥{m.Price}</span>
-              </div>
-            ))}
-          </div>
-          <div className="w-full flex flex-col justify-start border-t-2 pl-1">
-            {orderManager.menu.map((m, idx) => (
-              <div
-                key={idx}
-                className="w-full flex justify-between border-t-1 border-dashed px-2 py-1"
-              >
-                <span>{m.Item}</span>
-                <span className="font-normal">¥{m.Price}</span>
-              </div>
-            ))}
-          </div>
-          <div className="w-full flex flex-col justify-start border-t-2 pl-1">
-            {orderManager.menu.map((m, idx) => (
-              <div
-                key={idx}
-                className="w-full flex justify-between border-t-1 border-dashed px-2 py-1"
-              >
-                <span>{m.Item}</span>
-                <span className="font-normal">¥{m.Price}</span>
-              </div>
-            ))}
-          </div>
-          <div className="w-full flex flex-col justify-start border-t-2 pl-1">
-            {orderManager.menu.map((m, idx) => (
-              <div
-                key={idx}
-                className="w-full flex justify-between border-t-1 border-dashed px-2 py-1"
-              >
-                <span>{m.Item}</span>
-                <span className="font-normal">¥{m.Price}</span>
-              </div>
-            ))}
-          </div>
+            );
+          })}
         </div>
       </div>
 
